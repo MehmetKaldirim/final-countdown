4c5edad22b907403aeb3e636ae7983c61784fb40
Mehmet <mathdenizi@gmail.com>
2024-02-01 23:26:36 +0100

    Using Refs for DOM and others

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChalange.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Getting though" targetTime={10} />
+        <TimerChallenge title="Pro Only" targetTime={15} />
+      </div>
     </>
   );
 }
